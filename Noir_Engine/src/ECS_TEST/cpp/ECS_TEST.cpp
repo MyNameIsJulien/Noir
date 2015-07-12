58d6cca16a83cf8da970dd8b34d35156a690107e
J. Kirsch <t3ch13m4n@gmail.com>
2015-07-12 19:36:15 +0200

    Build is working. Gradle's a bug, when using Linkage:Shared

--- Noir_Engine/src/ECS_TEST/cpp/ECS_TEST.cpp
+++ Noir_Engine/src/ECS_TEST/cpp/ECS_TEST.cpp
@@ -1,8 +1,8 @@
-#include "World.hpp"
 
+#include "World.hpp"
 #include <iostream>
 
 int main(){
-	std::cout << "Starting ECS_TEST" << std::endl;#
+	std::cout << "Starting ECS_TEST" << std::endl;
 	return 0;
 }
